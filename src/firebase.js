5d9472be77be2cd4070c0963c891f3f135af5ecf
Assylzhan <asekabtw@gmail.com>
2024-02-12 09:15:10 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA63VZcEs3N3ftfMo8RCZp_1ezSI_BH358",
+  authDomain: "netflix-clone-20544.firebaseapp.com",
+  projectId: "netflix-clone-20544",
+  storageBucket: "netflix-clone-20544.appspot.com",
+  messagingSenderId: "573572058398",
+  appId: "1:573572058398:web:ba12dc4fb9ac4ba1904d0f",
+};
