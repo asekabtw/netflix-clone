7f3bc254a8b67e27816950e18eea2d63627cb573
Assylzhan <asekabtw@gmail.com>
2024-02-21 15:27:29 +0100

    pull api and style rows with hover effect

--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 
-function Row({ title, fetchUrl, isLageRow = false }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
   const base_url = "https://image.tmdb.org/t/p/original/";
@@ -23,16 +23,23 @@ function Row({ title, fetchUrl, isLageRow = false }) {
   return (
     <div className="row">
       <h2>{title}</h2>
-      {movies.map((movie) => {
-        return (
-          <img
-            src={`${base_url}${
-              isLageRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        );
-      })}
+
+      <div className="row__posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
     </div>
   );
 }
