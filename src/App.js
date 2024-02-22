fd9fec9f5d2306de0a0d1e6ca3afe18edb0c606c
Assylzhan <asekabtw@gmail.com>
2024-02-22 16:48:01 +0100

    add react router and screen folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <HomeScreen />
+
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
