56a1887e09f11c66004ee90b27823da6cbcdd58f
Assylzhan <asekabtw@gmail.com>
2024-03-04 18:31:01 +0100

    before firebase error

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA63VZcEs3N3ftfMo8RCZp_1ezSI_BH358",
   authDomain: "netflix-clone-20544.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "573572058398",
   appId: "1:573572058398:web:ba12dc4fb9ac4ba1904d0f",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+// import { auth } from "../firebase";
 import "./SignUpScreen.css";
 
 function SignUpScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+
+    // auth.createUserWithEmailAndPassword();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
 
         <h4>
           <span className="signupScreen__gray">New to Netflix? </span>
-          <span className="signupScreen__link">Sign Up Now.</span>
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up Now.
+          </span>
         </h4>
       </form>
     </div>
